7c767b94eb8d0163745a8ebe50389849cdcd9c1d
mike <mike.schoenbeck@gmail.com>
2020-01-16 19:33:07 -0500

    added more workOrder Controller methods. Updated all endpoints from using camelcase to using hyphens

--- src/main/java/com/zipcode/controllers/WorkOrderController.java
+++ src/main/java/com/zipcode/controllers/WorkOrderController.java
@@ -13,11 +13,14 @@ import java.time.LocalDate;
 import java.util.Date;
 
 @RestController
-@RequestMapping("/workOrders")
+@RequestMapping("/work-orders")
 public class WorkOrderController {
 
     @Autowired
     private WorkOrderService workOrderService;
+
+
+    //----------------------------------create----------------------------------
 
     @PostMapping("/create")
     public ResponseEntity createWorkOrder(@RequestBody WorkOrder workOrder)  {
@@ -25,17 +28,60 @@ public class WorkOrderController {
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
+    //----------------------------------get methods----------------------------------
+
     @GetMapping
     public ResponseEntity<Iterable<WorkOrder>> displayAllWorkOrders(){
         return new ResponseEntity<>(workOrderService.displayAllWorkOrders(), HttpStatus.OK);
     }
 
     @GetMapping("/{workOrderId}")
-    public ResponseEntity<WorkOrder> displayOneWorkOrder (@PathVariable Long workOrderId){
+    public ResponseEntity<WorkOrder> findWorkOrderById (@PathVariable Long workOrderId){
         return new ResponseEntity<>(workOrderService.findWorkOrderById(workOrderId), HttpStatus.OK);
     }
-    
-    @PutMapping("/{workOrderId}/updateStatus/")
+
+    @GetMapping("/work-orders-by-status")
+    public ResponseEntity <Iterable<WorkOrder>> findWorkOrdersByStatus( WorkOrderStatus workOrderStatus)   {
+        Iterable<WorkOrder> workOrders = workOrderService.findWorkOrdersByStatus(workOrderStatus);
+        if(workOrders == null)  {
+            throw new WorkOrderNotFoundException();
+        }
+        return new ResponseEntity<>(workOrders, HttpStatus.OK);
+    }
+
+    @GetMapping("/work-orders-by-date-created")
+    public ResponseEntity <Iterable<WorkOrder>> findWorkOrdersByDateCreated( LocalDate dateCreated) {
+        Iterable<WorkOrder> workOrders = workOrderService.findWorkOrdersByDateCreated(dateCreated);
+        if (workOrders == null) {
+            throw new WorkOrderNotFoundException();
+        }
+        return new ResponseEntity<>(workOrders, HttpStatus.OK);
+    }
+
+    @GetMapping("/work-orders-by-date-completed")
+    public ResponseEntity <Iterable<WorkOrder>> findWorkOrdersByDateCompleted( LocalDate dateCompleted) {
+        Iterable<WorkOrder> workOrders = workOrderService.findWorkOrdersByDateCompleted(dateCompleted);
+        if (workOrders == null) {
+            throw new WorkOrderNotFoundException();
+        }
+        return new ResponseEntity<>(workOrders, HttpStatus.OK);
+    }
+
+
+    @GetMapping("/work-orders-by-name")
+    public ResponseEntity<Iterable<WorkOrder>> findWorkOrdersByName(String lastname, String firstName)  {
+        Iterable<WorkOrder> workOrders = workOrderService.findWorkOrdersByName(lastname, firstName);
+        if(workOrders == null)  {
+            throw new WorkOrderNotFoundException();
+        }
+        return new ResponseEntity<>(workOrders, HttpStatus.OK);
+    }
+
+
+
+    //----------------------------------update methods----------------------------------
+
+    @PutMapping("/{workOrderId}/update-status/")
     public ResponseEntity<WorkOrder> updateWorkOrderStatus(@PathVariable Long  workOrderId, @RequestBody WorkOrderStatus workOrderStatus)    {
         WorkOrder workOrder = workOrderService.findWorkOrderById(workOrderId);
         if(workOrder == null)   {
@@ -45,7 +91,8 @@ public class WorkOrderController {
         return new ResponseEntity(HttpStatus.OK);
     }
 
-    @PutMapping("/{workOrderId}/updateDescription")
+
+    @PutMapping("/{workOrderId}/update-description")
     public ResponseEntity<WorkOrder> updateWorkOrderDescription(@PathVariable Long workOrderId, @RequestBody String workOrderDescription)   {
         WorkOrder workOrder = workOrderService.findWorkOrderById(workOrderId);
         if(workOrder == null)   {
@@ -55,16 +102,8 @@ public class WorkOrderController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @GetMapping("/workOrderByStatus")
-    public ResponseEntity <Iterable<WorkOrder>> findWorkOrdersByStatus( WorkOrderStatus workOrderStatus)   {
-        Iterable<WorkOrder> workOrders = workOrderService.findWorkOrdersByStatus(workOrderStatus);
-        if(workOrders == null)  {
-            throw new WorkOrderNotFoundException();
-        }
-        return new ResponseEntity<>(workOrders, HttpStatus.OK);
-    }
 
-    @PutMapping("/{workOrderId}/updateCreatedDate")
+    @PutMapping("/{workOrderId}/update-created-date")
     public ResponseEntity<WorkOrder> updateWorkOrderCreatedDate(@PathVariable Long workOrderId, @RequestBody LocalDate date)    {
         WorkOrder workOrder = workOrderService.findWorkOrderById(workOrderId);
         if(workOrder == null)   {
@@ -74,7 +113,7 @@ public class WorkOrderController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @PutMapping("/{workOrderId}/updateCompletedDate")
+    @PutMapping("/{workOrderId}/update-completed-date")
     public ResponseEntity<WorkOrder> updateWorkOrderCompletedDate(@PathVariable Long workOrderId, @RequestBody LocalDate date)  {
         WorkOrder workOrder = workOrderService.findWorkOrderById(workOrderId);
         if(workOrder == null)   {
@@ -82,6 +121,17 @@ public class WorkOrderController {
         }
         workOrderService.updateWorkOrderCompletedDate(workOrder, date);
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PutMapping("/{workOrderId}/update-location")
+    public ResponseEntity<WorkOrder> updateWorkOrderLocation(@PathVariable Long workOrderId, @RequestBody String location)  {
+        WorkOrder workOrder = workOrderService.findWorkOrderById(workOrderId);
+        if(workOrder == null)   {
+            throw new WorkOrderNotFoundException();
+        }
+        workOrderService.updateWorkOrderLocation(workOrder, location);
+        return new ResponseEntity<>(HttpStatus.OK);
+
     }
 
 
