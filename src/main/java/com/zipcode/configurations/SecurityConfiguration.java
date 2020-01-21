a5ddff8e2c9c6a4dba249c44c439c75b9aa202e3
mike <mike.schoenbeck@gmail.com>
2020-01-21 09:20:03 -0500

    rentered security bypass endpoints

--- src/main/java/com/zipcode/configurations/SecurityConfiguration.java
+++ src/main/java/com/zipcode/configurations/SecurityConfiguration.java
@@ -39,7 +39,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter implemen
     //configure to allow anyone to access some endpoints, all other needs to be authenticated
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/admin/create", "/authenticate","/admin/*", "/admins"
+        http.authorizeRequests().antMatchers("/admin/create", "/authenticate","/admin/*", "/admins", "/clients",
+                "/clients/register", "/clients/check-email", "/work-orders", "/work-orders/create",
+                "/ambassador-requests", "/ambassador-requests/request", "/socket"
                 ).permitAll()
                 .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .anyRequest().authenticated()
