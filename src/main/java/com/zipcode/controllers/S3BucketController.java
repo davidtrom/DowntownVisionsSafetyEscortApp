38a74e9a4aaccc1abe25e34820a87308162f9b27
Michael Schoenbeck <michael@Michaels-MBP.home>
2020-01-29 21:04:52 -0500

    minor changes

--- src/main/java/com/zipcode/controllers/S3BucketController.java
+++ src/main/java/com/zipcode/controllers/S3BucketController.java
@@ -1,10 +1,12 @@
 package com.zipcode.controllers;
 
 import com.zipcode.services.AmazonS3BucketService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
+
 
 
 @Controller
